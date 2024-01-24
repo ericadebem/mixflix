71ac83262c3878a8a53ecdf6f06dbbeeb8c50855
Erica de Bem <ericadebemm@gmail.com>
2024-01-24 22:26:53 +0100

    Add login form and buttons to LoginPage

---
+++ src/components/Authentication.tsx
@@ -0,0 +1,19 @@
+// import React, { useState } from "react";
+
+// export const Authentication = () => {
+//   const [username, setUsername] = useState("");
+//   const [password, setPassword] = useState("");
+
+//   const handleUsernameChange = (event) => {
+//     setUsername(event.target.value);
+//   };
+
+//   const handlePasswordChange = (event) => {
+//     setPassword(event.target.value);
+//   };
+
+//   const handleSubmit = async (event) => {
+//     event.preventDefault()
+//     ,,
+//   }
+// };
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,7 +1,23 @@
+import { Button } from "../components/Button";
+
+
 export const LoginPage = () => {
   return (
     <div>
       <h1>Login Page</h1>
+      <form> 
+        {/* onSubmit?? */}
+        <label htmlFor="username">Username/E-mail:</label>
+        <input type="text" name="username" id="username" />
+
+        <label htmlFor="password">Passwort:</label>
+        <input type="password" name="password" id="password" />
+
+        <Button text="Weiter" />
+        
+        <p>Neue hier?</p>
+        <Button text="Registrieren kostenlos" />
+      </form>
     </div>
   );
 };
